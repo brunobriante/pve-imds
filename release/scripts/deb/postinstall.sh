f2c5f2ba8254a22c2dc360fbc97fe1b7701fd0fd
Wyatt Anderson <wanderson@gmail.com>
2026-04-03 22:26:21 +0000

    fix: packaging/systemd service improvements

--- release/scripts/deb/postinstall.sh
+++ release/scripts/deb/postinstall.sh
@@ -1,4 +1,43 @@
 #!/bin/sh
+# postinst script for pve-imds
+
 set -e
-systemctl daemon-reload
-systemctl enable --now pve-imds.service
+
+case "$1" in
+    configure)
+
+        # The following line should be removed in trixie or trixie+1
+        deb-systemd-helper unmask 'pve-imds.service' >/dev/null || true
+
+        # was-enabled defaults to true, so new installations run enable.
+        if deb-systemd-helper --quiet was-enabled 'pve-imds.service'; then
+            # Enables the unit on first installation, creates new
+            # symlinks on upgrades if the unit file has changed.
+            deb-systemd-helper enable 'pve-imds.service' >/dev/null || true
+        else
+            # Update the statefile to add new symlinks (if any), which need to be
+            # cleaned up on purge. Also remove old symlinks.
+            deb-systemd-helper update-state 'pve-imds.service' >/dev/null || true
+        fi
+
+        if [ -d /run/systemd/system ]; then
+            systemctl --system daemon-reload >/dev/null || true
+            if [ -n "$2" ]; then
+                _dh_action=restart
+            else
+                _dh_action=start
+            fi
+            deb-systemd-invoke $_dh_action 'pve-imds.service' >/dev/null || true
+        fi
+    ;;
+
+    abort-upgrade|abort-remove|abort-deconfigure)
+    ;;
+
+    *)
+        echo "postinst called with unknown argument \`$1'" >&2
+        exit 1
+    ;;
+esac
+
+exit 0
