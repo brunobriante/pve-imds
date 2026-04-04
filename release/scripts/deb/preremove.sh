4ad8c51019f32f92e95c9cf220ea36625ac6ffd7
Wyatt Anderson <wanderson@gmail.com>
2026-04-04 23:24:02 +0000

    fix: unbreak prerm script

--- release/scripts/deb/preremove.sh
+++ release/scripts/deb/preremove.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 set -e
-systemctl disable --now pve-imds.service || true
+if [ -z "$DPKG_ROOT" ] && [ "$1" = remove ] && [ -d /run/systemd/system ] ; then
+        deb-systemd-invoke stop 'pve-imds.service' >/dev/null || true
+fi
